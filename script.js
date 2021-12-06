37d3a80193f604d315ba488b7d986226d7c884d9
Rando <67262768+SomeNormalRando@users.noreply.github.com>
2021-12-06 13:51:04 +0800

    small changes

--- script.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-for (const element of document.getElementsByClassName("github-link-button")) {
-	element.innerHTML = `<img src="./github-icon.webp" alt="GitHub Icon" width=30 height=30> ${element.innerHTML}`;
-}
-for (const link of document.querySelectorAll("a")) {
-	if (!link.title) link.title = link.innerText;
-}
